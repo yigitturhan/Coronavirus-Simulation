a9e4d049c3669e35d29e8e64cf256f83b61ed6f2
Ahmet Yiğit Turhan <112410497+yigitturhan@users.noreply.github.com>
2023-10-02 15:18:50 +0300

    Update the2.py (efficiency increased)

--- the2.py
+++ the2.py
@@ -1,7 +1,7 @@
 import math
 import random
 random.seed(111)
-inpt =  [100, 100, 5, 70, 30, 0.5, [[(37, 55), 6, 'notmasked', 'notinfected'], [(41, 78), 0, 'masked', 'notinfected'],
+inpt = [100, 100, 5, 70, 30, 0.5, [[(37, 55), 6, 'notmasked', 'notinfected'], [(41, 78), 0, 'masked', 'notinfected'],
                                    [(44, 8), 0, 'notmasked', 'notinfected'], [(47, 8), 0, 'masked', 'notinfected'],
                                    [(50, 15), 0, 'notmasked', 'notinfected'], [(53, 63), 0, 'masked', 'notinfected'],
                                    [(51, 48), 0, 'notmasked', 'notinfected'], [(20, 11), 6, 'notmasked', 'notinfected'],
@@ -18,18 +18,11 @@ inpt =  [100, 100, 5, 70, 30, 0.5, [[(37, 55), 6, 'notmasked', 'notinfected'], [
                                    [(23, 40), 3, 'masked', 'notinfected'], [(31, 43), 6, 'notmasked', 'notinfected'],
                                    [(53, 43), 2, 'notmasked', 'notinfected']]]
 universal_state = inpt[6]
-M = inpt[0]
-N = inpt[1]
-D = inpt[2]
-k = inpt[3]
-lmbda = inpt[4]
-mu = inpt[5]
-
+M , N , D , k , lmbda, mu = inpt[0], inpt[1], inpt[2] , inpt[3] , inpt[4], inpt[5]
 def new_move():
 	global universal_state
-	placeholder = [] #INITIAL VERSION
-	placeholderV2 = []   #version after movement
-	correctplaceholder = []   # correct version(i hope) (whether a place is full or whether this place is in universe)
+	placeholder , placeholderV2 = [] , [] #INITIAL VERSION and version after movement
+	correctplaceholder = []   # correct version(whether a place is full or whether this place is in universe)
 	lastmove = []  #firstversion
 	lastmoveV2 = []  #secondversion
 	maskstatus = [] #THIS LIST WILL NOT BE CHANGED
@@ -37,165 +30,30 @@ def new_move():
 	infectionstatusV3 = [] #AFTER INTERACTION
 	for i in universal_state:
 		placeholder.append(i[0])
-	for i in universal_state:
 		lastmove.append(i[1])
-	for i in universal_state:
 		maskstatus.append(i[2])
-	for i in universal_state:
 		infectionstatus.append(i[3])
-
 	def distance(c,d): #DISTANCE CALCULATOR
-		x1 = c[0]
-		x2 = d[0]
-		y1 = c[1]
-		y2 = d[1]
-		return math.sqrt((x2-x1)**2 + (y2-y1)**2)
+		return math.sqrt((d[0]-c[0])**2 + (d[1]-c[1])**2)
 	colors = ["green", "yellow2", "blue2", "purple2", "grey", "purple1", "blue1", "yellow1"]
-	possibilitiesofcolors = [mu/2,mu/8,0.5*(1-mu-mu**2),0.4*mu**2,0.2*mu**2,0.4*mu**2,0.5*(1-mu-mu**2),mu/8]
+	possibilitiesofcolors = [mu / 2, mu / 8, 0.5 * (1 - mu - mu ** 2), 0.4 * mu ** 2, 0.2 * mu ** 2, 0.4 * mu ** 2,
+							 0.5 * (1 - mu - mu ** 2), mu / 8]
+	color_offsets = {
+		"yellow1": (0, 1),
+		"green": (-1, 1),
+		"yellow2": (-1, 0),
+		"blue1": (1, 1),
+		"blue2": (-1, -1),
+		"purple1": (1, 0),
+		"grey": (1, -1),
+		"purple2": (0, -1),
+	}
 	# BEGINNING OF MOVEMENT PART
-
-	for i in universal_state :
-		a = random.choices(colors, weights=possibilitiesofcolors, k=1)
-		c = 0
-		x = i[0][0]
-		y = i[0][1]
-		if i[1] == 0:
-			if a[c] == "yellow1":
-				placeholderV2.append((x + 1,y + 1))
-			if a[c] == "green":
-				placeholderV2.append((x,y + 1))
-			if a[c] == "yellow2":
-				placeholderV2.append((x - 1,y + 1))
-			if a[c] == "blue1":
-				placeholderV2.append((x + 1,y))
-			if a[c] == "blue2":
-				placeholderV2.append((x - 1,y))
-			if a[c] == "purple1":
-				placeholderV2.append((x + 1,y - 1))
-			if a[c] == "grey":
-				placeholderV2.append((x,y - 1))
-			if a[c] == "purple2":
-				placeholderV2.append((x - 1,y - 1))
-		if i[1] == 1:
-			if a[c] == "yellow1":
-				placeholderV2.append((x, y + 1))
-			if a[c] == "green":
-				placeholderV2.append((x - 1, y + 1))
-			if a[c] == "yellow2":
-				placeholderV2.append((x - 1, y))
-			if a[c] == "blue1":
-				placeholderV2.append((x + 1, y + 1))
-			if a[c] == "blue2":
-				placeholderV2.append((x - 1, y - 1))
-			if a[c] == "purple1":
-				placeholderV2.append((x + 1, y))
-			if a[c] == "grey":
-				placeholderV2.append((x + 1, y - 1))
-			if a[c] == "purple2":
-				placeholderV2.append((x, y - 1))
-		if i[1] == 2:
-			if a[c] == "yellow1":
-				placeholderV2.append((x - 1, y + 1))
-			if a[c] == "green":
-				placeholderV2.append((x - 1, y))
-			if a[c] == "yellow2":
-				placeholderV2.append((x - 1, y - 1))
-			if a[c] == "blue1":
-				placeholderV2.append((x, y + 1))
-			if a[c] == "blue2":
-				placeholderV2.append((x, y - 1))
-			if a[c] == "purple1":
-				placeholderV2.append((x + 1, y + 1))
-			if a[c] == "grey":
-				placeholderV2.append((x + 1, y))
-			if a[c] == "purple2":
-				placeholderV2.append((x + 1, y - 1))
-		if i[1] == 3:
-			if a[c] == "yellow1":
-				placeholderV2.append((x - 1, y))
-			if a[c] == "green":
-				placeholderV2.append((x - 1, y - 1))
-			if a[c] == "yellow2":
-				placeholderV2.append((x, y - 1))
-			if a[c] == "blue1":
-				placeholderV2.append((x - 1, y + 1))
-			if a[c] == "blue2":
-				placeholderV2.append((x + 1, y - 1))
-			if a[c] == "purple1":
-				placeholderV2.append((x, y + 1))
-			if a[c] == "grey":
-				placeholderV2.append((x + 1, y + 1))
-			if a[c] == "purple2":
-				placeholderV2.append((x + 1, y))
-		if i[1] == 4:
-			if a[c] == "yellow1":
-				placeholderV2.append((x - 1, y - 1))
-			if a[c] == "green":
-				placeholderV2.append((x, y - 1))
-			if a[c] == "yellow2":
-				placeholderV2.append((x + 1, y - 1))
-			if a[c] == "blue1":
-				placeholderV2.append((x - 1, y))
-			if a[c] == "blue2":
-				placeholderV2.append((x + 1, y))
-			if a[c] == "purple1":
-				placeholderV2.append((x - 1, y + 1))
-			if a[c] == "grey":
-				placeholderV2.append((x, y + 1))
-			if a[c] == "purple2":
-				placeholderV2.append((x + 1, y + 1))
-		if i[1] == 5:
-			if a[c] == "yellow1":
-				placeholderV2.append((x, y - 1))
-			if a[c] == "green":
-				placeholderV2.append((x + 1, y - 1))
-			if a[c] == "yellow2":
-				placeholderV2.append((x + 1, y))
-			if a[c] == "blue1":
-				placeholderV2.append((x - 1, y - 1))
-			if a[c] == "blue2":
-				placeholderV2.append((x + 1, y + 1))
-			if a[c] == "purple1":
-				placeholderV2.append((x - 1, y))
-			if a[c] == "grey":
-				placeholderV2.append((x - 1, y + 1))
-			if a[c] == "purple2":
-				placeholderV2.append((x, y + 1))
-		if i[1] == 6:
-			if a[c] == "yellow1":
-				placeholderV2.append((x + 1, y - 1))
-			if a[c] == "green":
-				placeholderV2.append((x + 1, y))
-			if a[c] == "yellow2":
-				placeholderV2.append((x + 1, y + 1))
-			if a[c] == "blue1":
-				placeholderV2.append((x, y - 1))
-			if a[c] == "blue2":
-				placeholderV2.append((x, y + 1))
-			if a[c] == "purple1":
-				placeholderV2.append((x - 1, y - 1))
-			if a[c] == "grey":
-				placeholderV2.append((x - 1, y))
-			if a[c] == "purple2":
-				placeholderV2.append((x - 1, y + 1))
-		if i[1] == 7:
-			if a[c] == "yellow1":
-				placeholderV2.append((x + 1, y))
-			if a[c] == "green":
-				placeholderV2.append((x + 1, y + 1))
-			if a[c] == "yellow2":
-				placeholderV2.append((x, y + 1))
-			if a[c] == "blue1":
-				placeholderV2.append((x + 1, y - 1))
-			if a[c] == "blue2":
-				placeholderV2.append((x - 1, y + 1))
-			if a[c] == "purple1":
-				placeholderV2.append((x, y - 1))
-			if a[c] == "grey":
-				placeholderV2.append((x - 1, y - 1))
-			if a[c] == "purple2":
-				placeholderV2.append((x - 1, y))
-
+	for i in universal_state:
+		a = random.choices(colors, weights=possibilitiesofcolors, k=1)[0]
+		offset = color_offsets[a]
+		x, y = i[0][0] + offset[0], i[0][1] + offset[1]
+		placeholderV2.append((x, y))
 	for i in range(0,len(placeholder)):  #THIS WILL CHECK WHETHER THE PLACE IS EXİSTED OR OCCUPIED BEFORE
 		if (0 <= placeholderV2[i][0] < N) and (0 <= placeholderV2[i][1] < M) and (placeholderV2[i] not in placeholder[i+1:]) and (placeholderV2[i] not in placeholderV2[:i]):
 			correctplaceholder.append(placeholderV2[i])
@@ -203,41 +61,26 @@ def new_move():
 			placeholderV2[i] = placeholder[i]
 			correctplaceholder.append(placeholder[i])
 	#BEGINNING OF LASTMOVE PART
-
-	for i in correctplaceholder:
-		a = correctplaceholder.index(i)
-		if i == placeholder[a]:
-			lastmoveV2.append(lastmove[a])
-		else:
-			x1 = placeholder[a][0]
-			y1 = placeholder[a][1]
-			x2 = i[0]
-			y2 = i[1]
-			if x2-x1 == 0 and y2-y1 ==1:
-				lastmoveV2.append(0)
-			if x2-x1 == -1 and y2-y1 ==1:
-				lastmoveV2.append(1)
-			if x2-x1 == -1 and y2-y1 ==0:
-				lastmoveV2.append(2)
-			if x2-x1 == -1 and y2-y1 ==-1:
-				lastmoveV2.append(3)
-			if x2-x1 == 0 and y2-y1 ==-1:
-				lastmoveV2.append(4)
-			if x2-x1 == 1 and y2-y1 ==-1:
-				lastmoveV2.append(5)
-			if x2-x1 == 1 and y2-y1 ==0:
-				lastmoveV2.append(6)
-			if x2-x1 == 1 and y2-y1 ==1:
-				lastmoveV2.append(7)
+	lastmove_map = {
+		(0, 1): 0,
+		(-1, 1): 1,
+		(-1, 0): 2,
+		(-1, -1): 3,
+		(0, -1): 4,
+		(1, -1): 5,
+		(1, 0): 6,
+		(1, 1): 7,
+	}
+	lastmoveV2 = [lastmove_map((i[0] - placeholder[a][0], i[1] - placeholder[a][1])) for a, i in
+				  enumerate(correctplaceholder)]
 	#BEGINNING OF INFECTION PART
-
-	for i in range(0,len(infectionstatus)):
+	for i in range(len(infectionstatus)):
 		infectionstatusV2 = []
 		if infectionstatus[i] == "infected":
 			infectionstatusV3.append("infected")
 		if infectionstatus[i] == "notinfected":
-			for s in range(0,len(infectionstatus)):
-				if infectionstatus[s] == "infected" and distance(correctplaceholder[i],correctplaceholder[s]) <= D:
+			for s in range(len(infectionstatus)):
+				if infectionstatus[s] == "infected" and distance(correctplaceholder[i], correctplaceholder[s]) <= D:
 					if maskstatus[i] == "masked" and maskstatus[s] == "masked":
 						dstsqr = distance(correctplaceholder[i], correctplaceholder[s])**2
 						olslk = min(1, k / (dstsqr))/(lmbda**2)
